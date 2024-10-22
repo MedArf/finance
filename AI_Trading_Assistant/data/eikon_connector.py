41b766cba96f5a2d1a6230ce1281182e38b2b0df
MedArf <arfa.mehdi@gmail.com>
2024-10-22 16:02:02 +0200

    Cleaning Eikon RDP API testing

--- AI_Trading_Assistant/data/eikon_connector.py
+++
@@ -1,8 +0,0 @@
-import eikon as ek
-
-ek.set_app_key('c5859a4da1204b7cb109bdef864737b47e9f5013')
-
-
-df, err = ek.get_data(['GOOG.O', 'MSFT.O', 'FB.O', 'AMZN.O', 'TWTR.K'],['TR.Revenue.date','TR.Revenue','TR.GrossProfit'],{'Scale': 6, 'SDate': 0, 'EDate': -2, 'FRQ': 'FY', 'Curn': 'EUR'})
-
-print(df)
