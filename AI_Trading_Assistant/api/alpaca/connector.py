6a5cbb7f2c6aa3bfd384b509c4fe04a49fc67101
MedArf <arfa.mehdi@gmail.com>
2024-10-22 16:26:17 +0200

    Testing connection

---
+++ AI_Trading_Assistant/api/alpaca/connector.py
@@ -0,0 +1,20 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
+import configparser
+
+ALPACA_ENV=''
+def set_env():
+    global ALPACA_ENV
+    ALPACA_ENV="paper"
+#to be replaced with entries in DB
+
+set_env()
+config_file="/home/mehdi/Projects/finance/AI_Trading_Assistant/api/alpaca/config.json"
+config=configparser.ConfigParser()
+config.read(config_file)
+
+api_key=config[ALPACA_ENV+'api_keys']['APCA-API-KEY']
+api_secret=config[ALPACA_ENV+'api_keys']['APCA-API-SECRET']
+
+trading_client=TradingClient(api_key, api_secret)
+print(trading_client.get_account())
